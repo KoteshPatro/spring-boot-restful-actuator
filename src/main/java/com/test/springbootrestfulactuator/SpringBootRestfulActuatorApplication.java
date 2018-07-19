20a116ff9a51326882910bc75dc0684e33a4f12b
Kotesh <y.koteshpatro.gmail.com>
2018-07-19 14:59:08 +0530

    boot-actator

---
+++ src/main/java/com/test/springbootrestfulactuator/SpringBootRestfulActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.test.springbootrestfulactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulActuatorApplication.class, args);
+	}
+}
